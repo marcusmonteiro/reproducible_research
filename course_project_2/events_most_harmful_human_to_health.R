925490e58a2349e61b89e76dbd4cb2867f2ee3fd
Marcus Vinicius Monteiro de Souza <mvsouza007@gmail.com>
2017-06-09 16:49:44 -0300

    Add title and labels to plot

--- course_project_2/events_most_harmful_human_to_health.R
+++ course_project_2/events_most_harmful_human_to_health.R
@@ -11,4 +11,8 @@ most.harmful.event.types <- fatalities.plus.injuries.per.event.type %>%
 most.harmful.event.types %>%
   ggplot(aes(EVTYPE, FATALITIES_PLUS_INJURIES)) +
   geom_col(aes(fill = FATALITIES_PLUS_INJURIES == max(FATALITIES_PLUS_INJURIES)), show.legend = FALSE) +
-  coord_flip()
+  coord_flip() +
+  ggtitle('U.S.A, Storm Event Types Most Harmful to Human Health') +
+  ylab('Fatalities Plus Injuries') +
+  xlab('Event Type')
+
